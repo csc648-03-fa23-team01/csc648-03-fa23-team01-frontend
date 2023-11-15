847782e441355d2b1523499bc3fada2312861a3f
John42506176Linux <johnkongtcheu@gmail.com>
2023-11-15 13:08:25 -0800

    Finishing merge

--- my-react-app/src/reducers/RootReducers.jsx
+++ my-react-app/src/reducers/RootReducers.jsx
@@ -1,6 +1,7 @@
 // Import action types for both reducers
 import { SEARCH_REQUEST, SEARCH_SUCCESS, SEARCH_FAILURE } from "../actions/actionType";
 import { FETCH_TUTOR_REQUEST, FETCH_TUTOR_SUCCESS, FETCH_TUTOR_FAILURE } from "../actions/actionType";
+import { GET_USER_TUTORS_REQUEST, GET_USER_TUTORS_SUCCESS, GET_USER_TUTORS_FAILURE } from "../actions/actionType";
 import { combineReducers } from 'redux';
 
 // Search Reducer
@@ -71,9 +72,43 @@ export const tutorReducer = (state = tutorInitialState, action) => {
   }
 };
 
+const userInitialState = {
+    isLoading: false,
+    userData: null,
+    error: null,
+};
+
+export const userReducer = (state = userInitialState, action) => {
+    switch (action.type) {
+        case GET_USER_TUTORS_REQUEST:
+            return {
+                ...state,
+                isLoading: true,
+                error: null,
+            };
+        case GET_USER_TUTORS_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                userData: action.payload,
+                error: null,
+            };
+        case GET_USER_TUTORS_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                userData: null,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+  };
+
 const rootReducer = combineReducers({
   search: searchReducer,
-  tutor: tutorReducer
+  tutor: tutorReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
